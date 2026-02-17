cecbc2db77491b990a79999c3c750033058bd015
Lýz <147095077+Lyzurly@users.noreply.github.com>
2026-02-17 03:28:32 -0600

    web version works correctly!

--- Scripts/manage_minigames.gd
+++ Scripts/manage_minigames.gd
@@ -9,18 +9,30 @@ const _CALLBACK_GET_MINIGAME_DATA: String = \
 	"get_minigame_data"
 const _CALLBACK_GET_MINIGAME_SCENE_PATH: String = \
 	"get_minigame_scene_path"
+	
+enum _MINIGAME_DATA {PATH,CONFIG_NODE,MINIGAME_NODE}
+var _minigame_data_contents: Dictionary[_MINIGAME_DATA,Variant] = {
+	_MINIGAME_DATA.PATH: "",
+	_MINIGAME_DATA.CONFIG_NODE: null,
+	_MINIGAME_DATA.MINIGAME_NODE: null
+	}
 
 ## "minigame_name": "minigame_path"
-var _loadable_minigames: Dictionary[String,String] = {}
+var _loadable_minigames: Dictionary[String,Dictionary] = {}
 	
 var current_minigame_node: Node
 
 func _ready() -> void:
 	_ready_minigames()
+	#_ready_minigames_virtual()
+	
+func _ready_minigames_virtual() -> void:
+	_ready_minigame("minigame_thinking.pck")
 	
 func _ready_minigames() -> void:
 	var minigame_directory: DirAccess = \
 		DirAccess.open(_MINIGAME_DIRECTORY)
+	#push_error(_MINIGAME_DIRECTORY," load error: ",DirAccess.get_open_error())
 	for minigame_file_name: String in minigame_directory.get_files():
 		if minigame_file_name.ends_with(_PCK_STRING):
 			_ready_minigame(minigame_file_name)
@@ -30,19 +42,35 @@ func _ready_minigame(minigame_file_name: String) -> void:
 		_MINIGAME_DIRECTORY + minigame_file_name
 	var minigame_name: String = \
 		minigame_file_name.trim_suffix(_PCK_STRING)
-	_loadable_minigames.get_or_add(minigame_name,full_minigame_path)
+		
+	var minigame_data_dictionary: Dictionary[_MINIGAME_DATA,Variant] = \
+		_minigame_data_contents.duplicate()
+		
+	minigame_data_dictionary[_MINIGAME_DATA.PATH] = full_minigame_path
 	
-func get_minigame_data() -> Dictionary[String,String]:
+	_loadable_minigames.get_or_add(
+		minigame_name,minigame_data_dictionary)
+		
+	_load_minigame(minigame_name,full_minigame_path)
+	
+func get_minigame_data() -> Dictionary[String,Dictionary]:
 	return _loadable_minigames
 		
 func activate_minigame(indeed: bool,minigame_name: String) -> void:
+	var minigame_data_config_node: Node = \
+		_loadable_minigames[minigame_name][_MINIGAME_DATA.CONFIG_NODE]
 	if indeed:
-		var full_minigame_path: String = \
-			_loadable_minigames[minigame_name]
-		_load_minigame(minigame_name,full_minigame_path)
+		var instantiated_minigame: Node = \
+			_instantiate_minigame(
+				minigame_data_config_node)
+
+		_loadable_minigames[minigame_name][_MINIGAME_DATA.MINIGAME_NODE] = \
+			instantiated_minigame
 	else:
-		pass
-		
+		var minigame_node: Node = \
+			_loadable_minigames[minigame_name][_MINIGAME_DATA.MINIGAME_NODE]
+		minigame_node.queue_free()
+		_loadable_minigames[minigame_name][_MINIGAME_DATA.MINIGAME_NODE] = null
 		
 func _load_minigame(minigame_name:String,full_minigame_path: String) -> void:
 	var minigame_loaded: bool = \
@@ -53,6 +81,9 @@ func _load_minigame(minigame_name:String,full_minigame_path: String) -> void:
 	else:
 		push_error("Could not load file at ",full_minigame_path)
 	
+	_create_and_add_minigame_data_script(minigame_name)
+
+func _create_and_add_minigame_data_script(minigame_name: String) -> void:
 	## E.g. "res://minigame_thinking/minigame_thinking.gd"
 	var minigame_script_path: String = \
 		"res://" + minigame_name + "/" + minigame_name + ".gd"
@@ -60,13 +91,13 @@ func _load_minigame(minigame_name:String,full_minigame_path: String) -> void:
 		load(minigame_script_path)
 	var minigame_data_node: Node = minigame_data_script.new()
 	add_child(minigame_data_node)
-	
-	_instantiate_minigame(
-		minigame_data_node)
+
+	_loadable_minigames[minigame_name][_MINIGAME_DATA.CONFIG_NODE] = \
+		minigame_data_node
 		
 func _instantiate_minigame(
 minigame_data_node: Node
-) -> void:
+) -> Node:
 	var minigame_scene_path: String = \
 		minigame_data_node.call(_CALLBACK_GET_MINIGAME_SCENE_PATH)
 	var minigame_scene: PackedScene = \
@@ -79,6 +110,8 @@ minigame_data_node: Node
 		
 	_handle_minigame_callbacks_on_creation(
 		minigame_data["callback_data_on_creation"])
+		
+	return current_minigame_node
 
 func _handle_minigame_callbacks_on_creation(
 callback_dictionary: Dictionary
@@ -99,5 +132,3 @@ minigame_dir_name: String,
 	var marker_pos: Vector3 = Vector3(
 		minigame_marker.global_position)
 	current_minigame_node.global_position = marker_pos
-	
-	MinigameLauncher.ref.repopulate_tree()
--- Scripts/minigame_launcher.gd
+++ Scripts/minigame_launcher.gd
@@ -2,26 +2,20 @@ class_name MinigameLauncher extends CanvasLayer
 static var ref: MinigameLauncher
 func _init() -> void:
 	ref = self
-	
-@onready var _button_toggle_menu: Button = %"Button_Toggle-Menu"
-@onready var _launcher: MarginContainer = %Launcher_MarginContainer
+
 @onready var minigame_buttons_container: HFlowContainer = %MinigameButtons_HFlowContainer
-@onready var filesystem_tree: Tree = %FileSystem_Tree
 
 const _MINIGAME_BUTTON: PackedScene = preload("res://Scenes/minigame_launcher_button.tscn")
 
 func _ready() -> void:
 	_ready_launcher()
-	_ready_connections()
-	_ready_tree()
 	print(self.name," is ready")
 	
 func _ready_launcher() -> void:
-	_on_menu_toggled(false)
 	_ready_buttons()
 
 func _ready_buttons() -> void:
-	var minigame_data: Dictionary[String,String] = \
+	var minigame_data: Dictionary[String,Dictionary] = \
 		ManageMinigames.ref.get_minigame_data()
 	for minigame_name: String in minigame_data:
 		var new_button: Button = _MINIGAME_BUTTON.instantiate()
@@ -29,61 +23,17 @@ func _ready_buttons() -> void:
 		new_button.text = minigame_name.to_pascal_case()
 		new_button.toggled.connect(
 			_on_minigame_toggled.bind(
-				minigame_name))
-	if minigame_buttons_container.get_children().size() > 0:
-		_button_toggle_menu.visible = true
+				minigame_name,new_button))
+		_name_button(false,minigame_name,new_button)
 
-func _ready_connections() -> void:
-	_button_toggle_menu.toggled.connect(_on_menu_toggled)
-	
-func _ready_tree() -> void:
-	filesystem_tree.clear()
-	var res_string: String = "res://"
-	var res: DirAccess = DirAccess.open(res_string)
-	var dir_root: TreeItem = filesystem_tree.create_item()
-	dir_root.set_text(0,res_string)
-	_unpack_dir_to_tree(res,dir_root,res_string)
-
-func repopulate_tree() -> void:
-	_ready_tree()
-	
-func _unpack_dir_to_tree(
-dir_to_scan: DirAccess, parent: TreeItem, parent_string: String) -> void:
-	for directory_string: String in dir_to_scan.get_directories():
-		if directory_string.begins_with("."):
-			continue
-		#var partial_path_string: String = parent_string + directory_string + "/"
-		#print("Trying to open ",partial_path_string)
-		var actual_directory: DirAccess = \
-			DirAccess.open(directory_string)
-		#push_error(partial_path_string," load error: ",DirAccess.get_open_error())
-		var dir_root: TreeItem = filesystem_tree.create_item(parent)
-		dir_root.set_text(0,directory_string)
-		var child_dirs: PackedStringArray = actual_directory.get_directories()
-		var child_files: PackedStringArray = actual_directory.get_files()
-		if child_files.size() > 0 \
-		or child_dirs.size() > 0:
-			for child_dir: String in child_dirs:
-				var new_dir: TreeItem = \
-					filesystem_tree.create_item(dir_root)
-				new_dir.set_text(0,child_dir)
-				var path_string: String = parent_string + directory_string + "/" + child_dir
-				var actual_new_dir: DirAccess = \
-					DirAccess.open(path_string)
-				_unpack_dir_to_tree(actual_new_dir,new_dir,path_string)		
-			for child_file: String in child_files:
-				var new_file: TreeItem = \
-					filesystem_tree.create_item(dir_root)
-				new_file.set_text(0,child_file)	
-	
-func _on_menu_toggled(toggled: bool) -> void:
-	_launcher.visible = toggled
-	if toggled:
-		Camera.ref.change_camera_state(
-			Camera.CAMERA_STATES.DOWN)
+func _name_button(
+indeed: bool,minigame_name:String,minigame_button: Button) -> void:
+	if indeed:
+		minigame_button.text = "Remove " + minigame_name
 	else:
-		Camera.ref.change_camera_state(
-			Camera.CAMERA_STATES.DEFAULT)
+		minigame_button.text = "Load " + minigame_name
 
-func _on_minigame_toggled(toggled: bool,minigame_name:String) -> void:
+func _on_minigame_toggled(
+toggled: bool,minigame_name:String,minigame_button:Button) -> void:
 	ManageMinigames.ref.activate_minigame(toggled,minigame_name)
+	_name_button(toggled,minigame_name,minigame_button)
